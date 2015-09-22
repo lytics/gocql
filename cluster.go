c300c01d4b6d56c7f94df51ab531427bd11dc7a6
Aaron Raddon <aaron@lytics.io>
2015-09-22 09:51:53 -0700

    update import paths to lytics

--- cluster.go
+++ cluster.go
@@ -9,7 +9,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/gocql/gocql/lru"
+	"github.com/lytics/gocql/lru"
 )
 
 const defaultMaxPreparedStmts = 1000
--- session.go
+++ session.go
@@ -15,7 +15,7 @@ import (
 	"time"
 	"unicode"
 
-	"github.com/gocql/gocql/lru"
+	"github.com/lytics/gocql/lru"
 )
 
 // Session is the interface used by users to interact with the database.
